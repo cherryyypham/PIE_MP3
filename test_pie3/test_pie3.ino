1fdf6f07398a58d2632552883295ecbfd301bfbd
Cherry Pham <meokkmun@gmail.com>
2022-10-03 17:03:11 -0400

    In class Oct3 Work

--- test_pie3/test_pie3.ino
+++ test_pie3/test_pie3.ino
@@ -1,22 +1,38 @@
 
-int sensorPinA = A0;    // select the input pin for the potentiometer
-int sensorPinD = 2;
-int ledPin = 13;      // select the pin for the LED
-int sensorValueA = 0;  // variable to store the value coming from the sensor
-int sensorValueD = 0;
+int sensorPinLeft = A1;   // select the input pin for the IR sensor on the left
+int sensorPinRight = A0;  // select the input pin for the IR sensor on the right
+
+int sensorValueLeft = 0;  // variable to store the value coming from the sensor
+int sensorValueRight = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(sensorPinA, INPUT);
-  pinMode(sensorPinD, INPUT);
+  pinMode(sensorPinLeft, INPUT);
+  pinMode(sensorPinRight, INPUT);
+  
   Serial.begin(9600);
 }
 
 void loop() {
   // read the value from the sensor:
-  sensorValueA = analogRead(sensorPinA);
-  sensorValueD = digitalRead(sensorPinD);
-  Serial.println(sensorValueA);
-  delay(3000);
+  sensorValueLeft = analogRead(sensorPinLeft);
+  sensorValueRight = analogRead(sensorPinRight);
+  delay(500);
+  Serial.println(sensorValueLeft);
+  Serial.println(sensorValueRight);
+  // On: 900 sth
+  // Off: 700 sth
+  
+  if (sensorValueLeft < 850) {    //Off tape
+    if (sensorValueRight >= 850) {  //On tape
+      Serial.println("Turn left wheel, right wheel stationary");
+    }
+    else {
+      Serial.println("Turn right wheel, left wheel stationary");
+    }
+  }
+  else {
+    Serial.println("Go straight");
+  }
+  
   
 }
