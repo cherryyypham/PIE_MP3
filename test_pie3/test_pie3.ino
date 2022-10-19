a612ea9535de20439e9c9622858f44d79630fa1c
Cherry Pham <meokkmun@gmail.com>
2022-10-19 19:18:17 -0400

    Sad iteration that doesn't work

--- test_pie3/test_pie3.ino
+++ test_pie3/test_pie3.ino
@@ -1,10 +1,11 @@
-#include <Wire.h>
+//#include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
+// Naming the Adafruit Motor Shield as AFMS
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);   // initiating left motor
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);  // inititating right motor
 
 int sensorPinLeft = A1;   // select the input pin for the IR sensor on the left
 int sensorPinRight = A0;  // select the input pin for the IR sensor on the right
@@ -12,54 +13,109 @@ int sensorPinRight = A0;  // select the input pin for the IR sensor on the right
 int sensorValueLeft = 0;  // variable to store the value coming from the sensor
 int sensorValueRight = 0;  // variable to store the value coming from the sensor
 
+String input;       // variable to store input from .py file
+
 void setup() {
+  // initiating sensors
   pinMode(sensorPinLeft, INPUT);
   pinMode(sensorPinRight, INPUT);
 
-  AFMS.begin();
+  // initiating motorshield
+  AFMS.begin();           
+
+  // initial motor set-ups
   leftMotor->run(FORWARD);
   leftMotor->setSpeed(0);
   rightMotor->run(FORWARD);
   rightMotor->setSpeed(0);
-  
+
+  // begin serial - matching the serial number declared in python file
   Serial.begin(9600);
-  Serial.setTimeout(1);
 }
 
 void loop() {
   // read the value from the sensor:
   sensorValueLeft = analogRead(sensorPinLeft);
   sensorValueRight = analogRead(sensorPinRight);
-  Serial.println(sensorValueLeft);
-  Serial.println(sensorValueRight);
+
+  // print out sensor values - crucial to test if sensors are reading
+  // because our sensors' connection is bad
+//  Serial.println(sensorValueLeft);
+//  Serial.println(sensorValueRight);
   // On: 900 sth
   // Off: 700 sth
   
-  if (sensorValueLeft >= 850) {
-      Serial.println("Turn right wheel, left wheel stationary");
-      leftMotor->setSpeed(10);
-      rightMotor->setSpeed(50);
-  }
-  else if (sensorValueRight >= 850) {
-      leftMotor->setSpeed(50);
-      rightMotor->setSpeed(10);
-  }
-  else if (sensorValueLeft >= 850 and sensorValueRight >= 850) {
-
-    Serial.println("Sharp turnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn1");
-    leftMotor->setSpeed(250);
-    rightMotor->setSpeed(0);
-    delay(100);
-  }
   if (Serial.available() > 0) {
-    input = Serial.readString().toInt();
-    //leftMotor: 7xxx
-    //rightMotor: 8xxx
-    if (input / 1000 = 7) {
-      leftMotor->setSpeed(input%1000);
+    input = Serial.readStringUntil('\n');
+    Serial.write("RECEIVED");
+    leftMotor->setSpeed(30);
+    rightMotor->setSpeed(30);
+    if (input == "n") {
+      if (sensorValueRight >= 850) {
+//          Serial.println("Turn left wheel, right wheel stationary");
+          Serial.write("RIGHT");
+          leftMotor->setSpeed(50);
+          rightMotor->setSpeed(10);
+      }
+      else if (sensorValueLeft >= 850) {
+//          Serial.println("Turn right wheel, left wheel stationary");
+          Serial.write("LEFT");
+          leftMotor->setSpeed(10);
+          rightMotor->setSpeed(50);
+      }
+      
     }
-    if (input / 1000 = 8) {
-      rightMotor->setSpeed(input%1000);
+    if (input == "s") {
+      leftMotor->setSpeed(0);
+      rightMotor->setSpeed(0);      
     }
+//
+//
+//    if (input == "r") {
+//      if (sensorValueLeft >= 850) {
+////        Serial.println("Turn right wheel, left wheel stationary");
+//        leftMotor->setSpeed(0);
+//        rightMotor->setSpeed(40);
+//      }
+//      else if (sensorValueRight >= 850) {
+//          leftMotor->setSpeed(40);
+//          rightMotor->setSpeed(0);
+//      }
+//      else if (sensorValueLeft >= 850 and sensorValueRight >= 850) {
+//    
+////        Serial.println("Sharp turnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn1");
+//        leftMotor->setSpeed(250);
+//        rightMotor->setSpeed(0);
+//        delay(100);
+//      }
+//      else {
+////        Serial.println("Go straight");
+//        leftMotor->setSpeed(25);
+//        rightMotor->setSpeed(25);
+//      }
+//    if (input.charAt(0)== '7') {
+//      input = input.substring(1,4).toInt();
+//      leftMotor->setSpeed(input.toInt());
+//      rightMotor->setSpeed(0);
+//      Serial.write("LEFT");
+//      
+//    }
+//    if (input.charAt(0) == '8') {
+//      input = input.substring(1,4).toInt();
+//      rightMotor->setSpeed(input.toInt());
+//      leftMotor->setSpeed(0);
+//      Serial.write("RIGHT");
+//    }
+//    } 
   }
+//        if (sensorValueRight >= 850) {
+////          Serial.println("Turn left wheel, right wheel stationary");
+//          leftMotor->setSpeed(50);
+//          rightMotor->setSpeed(10);
+//      }
+//      else if (sensorValueLeft >= 850) {
+////          Serial.println("Turn right wheel, left wheel stationary");
+//          leftMotor->setSpeed(10);
+//          rightMotor->setSpeed(50);
+//      }
 }
--- test_send_data.py
+++ test_send_data.py
@@ -1,22 +1,44 @@
-# Importing Libraries
+# # Importing Libraries
+# import serial
+# import time
+
+# # Indicate the port connection between the device hosting the code and the Arduino board
+# arduinoComPort = "/dev/ttyACM0"
+# # Has to be the same baudRate with the code in the '.ino' file in order to take data from
+# # its serial monitor
+# baudRate = 9600
+# # Initiating serial port
+# arduino = serial.Serial(arduinoComPort, baudRate, timeout=1)
+
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.05)
+#     data = arduino.readline()
+#     return data
+
+# while True:
+# num = input("Enter a number: ") # Taking input from user
+# value = write_read(num)
+# print(type(num)) # printing the value
+
+# num = input ("Enter number :")
+# print(num)
+# name1 = input("Enter namSe : ")
+# print(name1)
+  
+# # Printing type of input value
+# print ("type of number", type(num))
+# print ("type of name", type(name1))
+
 import serial
 import time
-
-# Indicate the port connection between the device hosting the code and the Arduino board
-arduinoComPort = "/dev/ttyACM0"
-# Has to be the same baudRate with the code in the '.ino' file in order to take data from
-# its serial monitor
-baudRate = 9600
-# Initiating serial port
-arduino = serial.Serial(arduinoComPort, baudRate, timeout=1)
-
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
-
+arduino = serial.Serial("/dev/ttyACM1", 9600, timeout=1)
 while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
+    i = input("input(left/right motor value): ").strip()
+    if i == 'done':
+        print("Finished")
+        break
+    arduino.write(i.encode(encoding='utf-8'))
+    time.sleep(0.05)
+    print(arduino.readline().decode())
+arduino.close
