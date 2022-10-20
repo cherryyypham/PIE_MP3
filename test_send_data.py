9dfe8298b91b786fcaf814ef1eb8a83715171bf4
Cherry Pham <meokkmun@gmail.com>
2022-10-19 23:22:08 -0400

    Finished commented file

--- test_send_data.py
+++ test_send_data.py
@@ -1,44 +1,26 @@
-# # Importing Libraries
-# import serial
-# import time
-
-# # Indicate the port connection between the device hosting the code and the Arduino board
-# arduinoComPort = "/dev/ttyACM0"
-# # Has to be the same baudRate with the code in the '.ino' file in order to take data from
-# # its serial monitor
-# baudRate = 9600
-# # Initiating serial port
-# arduino = serial.Serial(arduinoComPort, baudRate, timeout=1)
-
-# def write_read(x):
-#     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
-
-# while True:
-# num = input("Enter a number: ") # Taking input from user
-# value = write_read(num)
-# print(type(num)) # printing the value
-
-# num = input ("Enter number :")
-# print(num)
-# name1 = input("Enter namSe : ")
-# print(name1)
-  
-# # Printing type of input value
-# print ("type of number", type(num))
-# print ("type of name", type(name1))
-
+# Importing Libraries
 import serial
 import time
+
+# Initiating serial port, integrating .ino file with this .py file
+# The first entry is the USB-port the Arduino is connected to
+# The second entry is the baudRate
 arduino = serial.Serial("/dev/ttyACM1", 9600, timeout=1)
+
+# Loop through and process input unless finish
 while True:
+    # Promp for input in terminal, take in string value
     i = input("input(left/right motor value): ").strip()
+    # If the input is 'done', stop the loop,
+    # not taking in more inputs
     if i == 'done':
         print("Finished")
         break
+    # Transfer input in the form of bytes to the Arduino
     arduino.write(i.encode(encoding='utf-8'))
+    # Processing time
     time.sleep(0.05)
+    # Print the output of arduino-processed data
     print(arduino.readline().decode())
+# Terminate connection with the Arduino after finished looping
 arduino.close
